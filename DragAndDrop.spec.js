c1eff76d045ff81fa55c3b13af018b20de05b18d
Marlon Dait <31147045+immarlondait@users.noreply.github.com>
2024-08-03 18:23:22 -0500

    Add files via upload

--- DragAndDrop.spec.js
+++ DragAndDrop.spec.js
@@ -2,8 +2,23 @@ const {test, expect} = require('@playwright/test')
 
 test('Drag and Drop', async ({page}) =>{
     
-    await page.goto('https://testautomationpractice.blogspot.com/')
+    await page.goto('http://dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
 
+    const seoul = await page.locator('#box5') //drag this item 
+    const southKorea = await page.locator('#box105') //drag to here
+
+    //Approach 1
+    // await seoul.hover()
+    // await page.mouse.down()
+    // await southKorea.hover()
+    // await page.mouse.up()
+
+
+    //Approach 2
+    await seoul.dragTo(southKorea)
+
+
+    
     
 
     await page.waitForTimeout(2000) //pausing execution, give you time to get eyes on it
