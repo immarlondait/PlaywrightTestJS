64c5cc0fade89fa1b0fe16f3565afe7b55b0a1d7
Marlon Dait <31147045+immarlondait@users.noreply.github.com>
2024-08-12 14:20:40 -0500

    Add files via upload

---
+++ Reporters.spec.js
@@ -0,0 +1,22 @@
+//https://playwright.dev/docs/test-reporters 
+const {test, expect} = require('@playwright/test')
+
+test('Test1', async ({page}) => {
+    await page.goto('https://www.demoblaze.com/index.html')
+    await expect(page).toHaveTitle('STORE')
+})
+
+test('Test2', async ({page}) => {
+    await page.goto('https://demo.opencart.com/')
+    await expect(page).toHaveTitle('Your Store')
+})
+
+test('Test3', async ({page}) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await expect(page).toHaveTitle('OrangeHRM')
+})
+
+// changed config reporters from 'html' to 'list'
+// or manually via terminal
+//                    --reporter=list
+// line, list, dot, html, json, junit, github (actions annotations)
