8926970c94b228f4b9e401ca3193bac32a47208a
Marlon Dait <31147045+immarlondait@users.noreply.github.com>
2024-08-14 10:48:04 -0500

    Add files via upload

---
+++ APITests.spec.js
@@ -0,0 +1,17 @@
+const {test, expect} = require('@playwright/test')
+
+test('Get users', async ({request}) => {
+    
+})
+
+test('Create user', async ({request}) => {
+    
+})
+
+test('Update user', async ({request}) => {
+    
+})
+
+test('Delete user', async ({request}) => {
+    
+})
